a04827f1ab60aea0ed4bbd4135b16ff4ba76ff3f
Eugene <eugenefromrus@gmail.com>
2021-07-26 00:17:13 +1000

    Промежуточный

--- src/assets/scripts/default.js
+++ src/assets/scripts/default.js
@@ -6,11 +6,13 @@ if (sidebar && header && menuBar) {
 
   menuBar.addEventListener('click', function (e) {
     header.classList.toggle('active');
+    document.body.classList.toggle('hidden');
   });
 
   sidebar.addEventListener('click', function (e) {
     if (e.target === this) {
       header.classList.remove('active');
+      document.body.classList.remove('hidden');
     }
   });
 }
