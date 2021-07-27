5c5d5e118c70c522ca50bd10e98ae29643e46a71
Eugene <eugenefromrus@gmail.com>
2021-07-27 20:38:56 +1000

    Наши публикация моб.версия

--- src/assets/scripts/home.js
+++ src/assets/scripts/home.js
@@ -1,22 +1,51 @@
-const featuresSwiper = document.getElementById('featuresSwiper');
-if (featuresSwiper) {
-  const swiperOptions = {
-    slidesPerView: 1,
-    loop: true,
-    pagination: {
-      el: '.swiper-pagination',
-      type: 'bullets',
-      clickable: true
-    },
-    on: {
-      init: function () {
-        console.log('swiper initialized');
+// Наши преимущества
+(() => {
+  const featuresSwiper = document.getElementById('featuresSwiper');
+  if (featuresSwiper) {
+    const swiperOptions = {
+      slidesPerView: 1,
+      spaceBetween: 10,
+      loop: true,
+      pagination: {
+        el: '#featuresSwiper .swiper-pagination',
+        type: 'bullets',
+        clickable: true
       },
-    },
-  };
+      on: {
+        init: function () {
+          console.log('swiper initialized');
+        }
+      }
+    };
 
-  const featuresSwiperSlider = new Swiper('#featuresSwiper', swiperOptions);
-  if (window.innerWidth >= 640) {
-    featuresSwiperSlider.destroy(true, true);
+    const featuresSwiperSlider = new Swiper(featuresSwiper, swiperOptions);
+    if (window.innerWidth >= 640) {
+      featuresSwiperSlider.destroy(true, true);
+    }
   }
-}
+})();
+
+
+// Наши публикации
+(()=>{
+  const publicationsSwiper = document.getElementById('publicationsSwiper');
+  if (publicationsSwiper) {
+    const swiperOptions = {
+      slidesPerView: 1,
+      spaceBetween: 10,
+      loop: true,
+      pagination: {
+        el: '.publications__bottom .swiper-pagination',
+        type: 'bullets',
+        clickable: true
+      },
+      on: {
+        init: function () {
+          console.log('swiper initialized');
+        }
+      }
+    };
+
+    const publicationsSwiperSlider = new Swiper(publicationsSwiper, swiperOptions);
+  }
+})()
