dbbc196b1258053314d5a49c3b9f59f511db60ae
Eugene <eugenefromrus@gmail.com>
2021-08-07 16:03:05 +1000

    added feature for header

--- src/assets/scripts/default.js
+++ src/assets/scripts/default.js
@@ -2,6 +2,17 @@ const sidebar = document.getElementById('sidebar');
 const header = document.getElementById('header');
 const menuBar = document.getElementById('menuBar');
 
+window.addEventListener('scroll', function(e) {
+  let offsetTop = window.scrollY;
+ 
+  if (offsetTop > 0 && window.innerWidth >= 1915) {
+    header.classList.add('scrolled')
+  } else {
+    header.classList.remove('scrolled')
+
+  }
+})
+
 if (sidebar && header && menuBar) {
 
   menuBar.addEventListener('click', function (e) {
