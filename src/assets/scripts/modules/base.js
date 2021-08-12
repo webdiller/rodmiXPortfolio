fd38fdc95f0e7c1125803f1cad233ff7a18a72bd
Eugene <eugenefromrus@gmail.com>
2021-08-13 09:37:17 +1000

    правки

--- src/assets/scripts/modules/base.js
+++ src/assets/scripts/modules/base.js
@@ -5,38 +5,22 @@ export function defaultScripts() {
   const menuClose = document.getElementById('menuClose');
 
   const callbackModal = document.getElementById('callbackModal');
+  const callbackModalClose = document.getElementById('callbackModalClose');
+  
   const callbackModalButtons = document.querySelectorAll('[data-id="callbackModal"]');
-
-  const orderModal = document.getElementById('orderModal');
-  const orderModalButtons = document.querySelectorAll('[data-id="orderModal"]');
-
   const callbackSuccess = document.getElementById('callbackSuccess');
-  const callbackSuccessButtons = document.querySelectorAll('[data-id="callbackSuccess"]');
 
-  const orderForm = document.getElementById('orderForm');
   const callbackForm = document.getElementById('callbackForm');
-
-  if (orderForm) {
-    orderForm.addEventListener('submit', function (e) {
-      e.preventDefault();
-      orderModal.classList.remove('active');
-      setTimeout(() => {
-        callbackSuccess.classList.add('active');
-      }, 250);
-    });
-  }
 
   if (callbackForm) {
     callbackForm.addEventListener('submit', function (e) {
       e.preventDefault();
-      callbackModal.classList.remove('active');
       setTimeout(() => {
         callbackSuccess.classList.add('active');
       }, 250);
     });
   }
 
-  // callbackModal
   if (callbackModalButtons && callbackModal) {
     callbackModalButtons.forEach((element) => {
       element.addEventListener('click', function () {
@@ -51,35 +35,11 @@ export function defaultScripts() {
     });
   }
 
-  // orderModal
-  if (orderModalButtons && orderModal) {
-    orderModalButtons.forEach((element) => {
-      element.addEventListener('click', function () {
-        orderModal.classList.toggle('active');
-      });
-    });
+  callbackModalClose.addEventListener('click', function (e) {
+    callbackSuccess.classList.remove('active')
+    callbackModal.classList.remove('active')
+  })
 
-    orderModal.addEventListener('click', function (e) {
-      if (e.target === this) {
-        orderModal.classList.remove('active');
-      }
-    });
-  }
-
-  // callbackSuccess
-  if (callbackSuccessButtons && callbackSuccess) {
-    callbackSuccessButtons.forEach((element) => {
-      element.addEventListener('click', function () {
-        callbackSuccess.classList.toggle('active');
-      });
-    });
-
-    callbackSuccess.addEventListener('click', function (e) {
-      if (e.target === this) {
-        callbackSuccess.classList.remove('active');
-      }
-    });
-  }
 
   window.addEventListener('scroll', function (e) {
     if (window.scrollY > 0 && window.innerWidth >= 1915) {
