589fb8786cdbfe15118a5ae5ced499c8c8f5e4ee
eugene <eugenefromrus@gmail.com>
2021-09-08 09:57:21 +1000

    промежуточный

--- src/assets/scripts/modules/uiForm.js
+++ src/assets/scripts/modules/uiForm.js
@@ -1,15 +1,17 @@
 const forms = document.querySelectorAll('[data-ui-form]');
 
 export const uiForms = () => {
-  const forms = document.querySelectorAll('[data-ui-form]');
-  if (forms.length > 0) {
-    forms.forEach((element) => {
-      const btnSubmit = element.querySelector('button[type="button"]');
-      if (btnSubmit) {
-        btnSubmit.addEventListener('click', function (e) {
-          this.previousElementSibling.classList.toggle('active');
-        });
-      }
-    });
+  if (window.innerWidth <= 479) {
+    const forms = document.querySelectorAll('[data-ui-form]');
+    if (forms.length > 0) {
+      forms.forEach((element) => {
+        const btnSubmit = element.querySelector('button[type="button"]');
+        if (btnSubmit) {
+          btnSubmit.addEventListener('click', function (e) {
+            this.previousElementSibling.classList.toggle('active');
+          });
+        }
+      });
+    }
   }
 };
