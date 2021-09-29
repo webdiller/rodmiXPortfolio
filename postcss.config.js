e507e27037d8f72188965ce61e80f285a6589273
eugene <eugenefromrus@gmail.com>
2021-09-29 14:28:28 +1000

    промежуточный

--- postcss.config.js
+++ postcss.config.js
@@ -5,7 +5,7 @@ module.exports = {
             extensions: ".scss"
         }),
         require("autoprefixer")({
-            browsers: ["last 2 versions"],
+            browsers: ["last 4 versions"],
             cascade: false
         }),
         require("postcss-advanced-variables")({
