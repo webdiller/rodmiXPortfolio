a4c51e15d320c9dac8821c292a0293492cad5962
eugene <eugenefromrus@gmail.com>
2021-10-11 12:05:51 +1000

    промежуточные правки

--- src/assets/scripts/modules/youtube.js
+++ src/assets/scripts/modules/youtube.js
@@ -1,10 +1,23 @@
 export const youtube = () => {
+  
+  const youtubeClose = document.getElementById('youtubeClose');
   const youtubeTrigger = document.getElementById('youtubeTrigger');
   const youtubeTarget = document.getElementById('youtubeTarget');
-
+  const youtubeIframe = document.getElementById('youtubeIframe');
+  const youtubeOverlay = document.getElementById('youtubeOverlay');
+  
   if (youtubeTrigger && youtubeTarget) {
+
     youtubeTrigger.addEventListener('click', function (e) {
       youtubeTarget.classList.add('active');
+      youtubeOverlay.classList.add('active');
     });
+
+    youtubeClose.addEventListener('click', function (e) {
+      youtubeTarget.classList.remove('active');
+      youtubeIframe.src = youtubeIframe.src;
+      youtubeOverlay.classList.remove('active');
+    });
+    
   }
 };
